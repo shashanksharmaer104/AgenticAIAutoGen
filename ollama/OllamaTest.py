f6b0a8193dc25801cc21adee9c2c399cee2dbb9a
Shashank Sharma <shashanksharma.er104@gmail.com>
2026-01-15 20:42:00 +0530

    Added loading animation capability in ollama test class

--- ollama/OllamaTest.py
+++ ollama/OllamaTest.py
@@ -5,30 +5,39 @@ from autogen_agentchat.ui import Console
 from autogen_core.models import ModelInfo
 from autogen_ext.models.ollama import OllamaChatCompletionClient
 from dotenv import load_dotenv
+from halo import Halo
 
 load_dotenv()
 
 async def main():
+
+    spinner = Halo(text="AutoGen is thinking...", spinner="dots")
+    spinner.start()
+
+    try:
     # Define model info for gemma3
-    model_info = ModelInfo(
-        vision=False,
-        function_calling=True,
-        json_output=True,
-        family="gemma"
-    )
-    
-    ollama_client = OllamaChatCompletionClient(
-        model="gemma3:4b",
-        model_info=model_info,
-    )
+        model_info = ModelInfo(
+            vision=False,
+            function_calling=True,
+            json_output=True,
+            family="gemma"
+        )
 
-    assistant = AssistantAgent(
-        name="MathTutor",
-        model_client=ollama_client,
-        system_message="You are a expert and helpful math tutor who can solve some complex math equations and problems."
-    )
+        ollama_client = OllamaChatCompletionClient(
+            model="gemma3:4b",
+            model_info=model_info,
+        )
 
-    await Console(assistant.run_stream(task="Solve this linear equation. Find value of x: 4x-7(2-x)=3x+2"))
-    await ollama_client.close()
+        assistant = AssistantAgent(
+            name="MathTutor",
+            model_client=ollama_client,
+            system_message="You are a expert and helpful math tutor who can solve some complex math equations and problems."
+        )
+
+        await Console(assistant.run_stream(task="Solve this linear equation. Find value of x: 4x-7(2-x)=3x+2"))
+        await ollama_client.close()
+
+    finally:
+        spinner.stop()
 
 asyncio.run(main())
